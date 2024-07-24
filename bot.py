3f3df2a43ed0760a5860cefc0dd299e0820eeb72
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2024-07-24 11:57:07 -0300

    demontração linkedin

--- bot.py
+++ bot.py
@@ -82,8 +82,8 @@ for n in np.ones(quantidade_notas):
             not_found("botao_janela_impressao")
         bot.click()
 
-        if not bot.find("botao_ok_impressao.png", matching=0.97, waiting_time=10000):
-            not_found("botao_ok_impressao")
+        if not bot.find("botao_cancelar(teste).png", matching=0.97, waiting_time=10000):
+            not_found("botao_cancelar(teste)")
         bot.click()
         
         if not bot.find("botao_fechar_lancamento", matching=0.97, waiting_time=10000):
