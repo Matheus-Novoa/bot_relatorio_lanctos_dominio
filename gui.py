8bb6eed686209d6a87124e77b4143ae51d4217f7
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2024-11-21 08:56:09 -0300

    Estilizando a interface

--- gui.py
+++ gui.py
@@ -3,29 +3,53 @@ from bot import main
 
 
 
-ctk.set_appearance_mode('system')
+# ctk.set_appearance_mode('dark')
+# ctk.set_default_color_theme('dark-blue')
 
 janela = ctk.CTk()
-janela.geometry('500x200')
+janela.geometry('370x170')
+janela.title('Imprimir relatórios')
 
-textoLoteInicial = ctk.CTkLabel(janela, text='Número do Lote').pack()
-numeroLoteInicial = ctk.CTkEntry(janela, placeholder_text='lote')
-numeroLoteInicial.pack()
+padx = 10
+pady= 5
+entryWidth = 180
 
-textoMes = ctk.CTkLabel(janela, text='Mês').pack()
-mes = ctk.CTkEntry(janela, placeholder_text='mes')
-mes.pack()
+#--------------- Número do Lote Inicial ------------------------
+textoLoteInicial = ctk.CTkLabel(janela, text='Número do Lote')
+textoLoteInicial.grid(row=0, column=0, padx=padx, pady=pady)
 
-textoQuantidade = ctk.CTkLabel(janela, text='Quantidade de Lotes').pack()
-quantLotes = ctk.CTkEntry(janela)
-quantLotes.pack()
+textPlaceholderLote = 'Insira o número do lote'
+numeroLoteInicial = ctk.CTkEntry(janela,
+                                 placeholder_text=textPlaceholderLote,
+                                 width=entryWidth)
+numeroLoteInicial.grid(row=0, column=1, padx=padx, pady=pady)
+#-----------------------------------------------------------------------
+#------------------ Mês -------------------------------
+textoMes = ctk.CTkLabel(janela, text='Mês')
+textoMes.grid(row=1, column=0, padx=padx, pady=pady)
 
+textPlaceholderMes = 'Insira o mês de exercício'
+mes = ctk.CTkEntry(janela,
+                   placeholder_text=textPlaceholderMes,
+                   width=entryWidth)
+mes.grid(row=1, column=1, padx=padx, pady=pady)
+#-------------------------------------------------------------
+#-------------------- Quantidade -----------------------------------
+textoQuantidade = ctk.CTkLabel(janela, text='Quantidade de Lotes')
+textoQuantidade.grid(row=2, column=0, padx=padx, pady=pady)
+
+textPlaceholderQuant = 'Insira a quantidade de lotes'
+quantLotes = ctk.CTkEntry(janela,
+                          placeholder_text=textPlaceholderQuant,
+                          width=entryWidth)
+quantLotes.grid(row=2, column=1, padx=padx, pady=pady)
+#-------------------------------------------------------------------
 def gatilho():
     janela.iconify()
     main(int(numeroLoteInicial.get()), mes.get(), int(quantLotes.get()))
     janela.destroy()
 
 botao = ctk.CTkButton(janela, text='Iniciar', command=gatilho)
-botao.pack()
+botao.grid(row=3, column=1, padx=padx, pady=20)
 
 janela.mainloop()
